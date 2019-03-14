eced0f19c2ee67da7aae5da6f8b04676b7a14c18
visualstudio <visualstudio@230-PC43>
2019-03-14 13:41:37 -0400

    comit

--- bank1/Class1.cs
+++ bank1/Class1.cs
@@ -1,8 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace bank1
+namespace bank
 {
-    public class Class1
+    public class BankAccount
     {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+        private BankAccount()
+        {
+
+        }
     }
+    public BankAccount(string customerName, double balance)
+    {
+        m_customerName = customerName;
+        m_balance = balance;
+
+
+    }
+    public string CustomerName
+    {
+        get { return m_customerName }
+    }
+    public double Balance
+    {
+        get { return m_balance}
+    }
+    public void Debit(double amount)
+    {
+        if (m_frozen)
+        {
+            throw new ArgumentOutOfRangeException("Acoount frozen");
+        }
+        if (amount > m_balance)
+        {
+            throw new ArgumentOutOfRangeException("amount");
+        }
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException("amount");
+        }
+        m_balance += amount;
+    }
+    public void Credit(double amount)
+    {
+        if (m_frozen)
+        {
+            throw new Exception("Account frozen");
+        }
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException("amount");
+        }
+        m_balance += amount;
+    }
+    private void FreezeAccount()
+    {
+        m_frozen = true;
+    }
+    private void UnfreezeAccount()
+    {
+        m_frozen = false;
+    }
+    public static void Maiin()
+    {
+        BankAccount ba = new BankAccount("Mr.Bryan Walton", 11.99);
+        ba.Credit(5.77);
+        ba.Debit(11.22);
+        Console > WriteLine("current balance is 4{0}", ba.Balance);
+    }
+}
 }
